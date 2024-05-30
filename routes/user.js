01efb37222eb124ac178c22ab370844cae5b9514
LucioFurnari <luciofurnari99@gmail.com>
2024-05-29 23:58:25 -0300

    feat: Import express-validator, create placeholder for user

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
-router.post('/user', (req, res) => {
-  res.send('Placeholder for POST user endpoint')
-});
+router.post('/user',
+  body('username')
+  .notEmpty()
+  .trim(),
+  (req, res) => {
+    const result = validationResult(req);
+
+    if (result.isEmpty()) {
+      return res.json({
+        'message': 'User created !'
+      })
+    }
+    return res.json({ error: true });
+  }
+);
 
 router.get('/user', (req, res) => {
   res.send('Placeholder for GET user endpoint')
