dba45db0512c3442e6b24f14941d9a0e079bfb80
LucioFurnari <luciofurnari99@gmail.com>
2024-05-30 22:08:46 -0300

    feat: create function to generate hash and validate password

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,5 +1,6 @@
 import { body, validationResult } from "express-validator";
 import { Request } from "express-validator/lib/base";
+import bcrypt, { hash } from 'bcrypt';
 
 export const createUser = [
   body('username').trim().notEmpty(),
@@ -23,3 +24,19 @@ export const getUser = async (_req: Request, res: { json: (arg0: { user: string;
     user: 'User data',
   })
 };
+
+async function generateHash() {
+  bcrypt.hash('B4c0/\/', 8, (error, hash) => {
+    console.log(hash);
+  });
+};
+
+async function checkPassword(hash: string) {
+  bcrypt.compare('B4c0/\/', hash, (error, res) => {
+    if (res) {
+      // something
+    }
+    console.log(error);
+    // res => true or false
+  });
+}
