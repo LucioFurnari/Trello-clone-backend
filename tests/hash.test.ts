d01dab9f65519c840dba409290776c623489c238
LucioFurnari <luciofurnari99@gmail.com>
2024-05-31 18:41:38 -0300

    feat: create two tests

--- tests/hash.test.ts
+++ tests/hash.test.ts
@@ -1,7 +1,31 @@
-import { simpleMath } from "../controllers/user";
+import { server } from '..'
+import request from 'supertest'
 
-describe('testing math function', () => {
-  test('empty string should result in zero', () => {
-    expect(simpleMath(1,2)).toEqual(3)
-  })
+describe('Get user data', function() {
+  it('Should return a 200 status and a message', async function() {
+    const response = await request(server)
+      .get('/api/user')
+    
+    expect(response.statusCode).toBe(200)
+    expect(response.body.user).toBe('User data')
+  });
 });
+
+afterAll(done => {
+  server.close()
+  done()
+})
+
+describe('Create user with form', function() {
+  it('Should create a new user', async function() {
+    const response = await request(server)
+      .post('/api/user')
+      .send({
+        username: "john",
+        email: "johnOG@gmail.com",
+        password: "74116"
+      })
+    expect(response.statusCode).toBe(200)
+    expect(response.body.message).toBe('User created!')
+  })
+})
