8452acc4c9ca027fd56f406831b59239792897a9
LucioFurnari <luciofurnari99@gmail.com>
2024-06-02 23:40:29 -0300

    feat: create card controller file

---
+++ controllers/card.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+export async function getCard(_req: express.Request, _res: express.Response) {
+  // Get card 
+}
+
+export async function createCard() {
+  // Create new card
+}
+
+export async function updateCard() {
+  // Update card
+}
+
+export async function deleteCard() {
+  // Delete card
+}
