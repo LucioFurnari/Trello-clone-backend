ca1da5cd4e6ffde2927a6cb172f5d6bc8d4a9eb3
LucioFurnari <luciofurnari99@gmail.com>
2024-06-04 22:59:18 -0300

    feat: create new functions

--- controllers/board.ts
+++ controllers/board.ts
@@ -3,3 +3,15 @@ import express from 'express';
 export async function getBoard(_req: express.Request, _res: express.Response) {
   // Get board from request.board_id
 }
+
+export async function createBoard() {
+  
+}
+
+export async function updateBoard() {
+  
+}
+
+export async function deleteBoard() {
+  
+}
