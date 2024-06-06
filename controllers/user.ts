e0eee919bcc62ad7f7ab47428f99e01296269edb
LucioFurnari <luciofurnari99@gmail.com>
2024-06-05 23:52:37 -0300

    feat: create new function, and use bcrypt for password

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,21 +1,35 @@
 import { body, validationResult } from "express-validator";
+import { PrismaClient } from "@prisma/client";
 import bcrypt from 'bcrypt';
 import express from 'express';
+
+const prisma = new PrismaClient();
 
 export const createUser = [
   body('username').trim().notEmpty(),
   body('email').isEmail().notEmpty(),
   body('password').trim().notEmpty(),
-  async (req: express.Request, res: express.Response ) => {
+  async (req: express.Request, res: express.Response) => {
+    const { username, email, password} = req.body;
     const result = validationResult(req);
 
     if (result.isEmpty()) {
+      bcrypt.hash(password, 8, async (_error, hash) => {
+        await prisma.user.create({
+          data: {
+            name: username,
+            email: email,
+            password: hash
+          }
+        })
+      })
+
       return res.status(200).json({
         message: 'User created!',
       })
     }
 
-    return res.json({ error: true, list: JSON.stringify(result)})
+    return res.status(400).json({ error: true, errorList: JSON.stringify(result)});
   },
 ];
 
@@ -29,9 +43,39 @@ export async function updateUserData() {
   
 }
 
-export async function loginUser() {
+export const loginUser = [
+  body('email').trim().notEmpty().isEmail(),
+  async (req: express.Request, res: express.Response) => { 
+    const { email, password } = req.body;
+    const result = validationResult(req);
+
+    if (result.isEmpty()) {
+      const user = await prisma.user.findFirst({
+        where: {
+          email: email
+        }
+      })
+
+      if (user) {
+        bcrypt.compare(password, user?.password, (err, result) => {
+          if (err) {
+            // Handle error
+            console.error('Error comparing passwords:', err);
+            return;
+          }
   
-}
+          if (result) {
+            return res.json({ message: 'User logged!'});
+          } else {
+            return res.json({ message: 'The password is incorrect'});
+          }
+        })
+      }
+    }
+
+    return res.status(400).json({ error: true, errorList: JSON.stringify(result)});
+  }
+]
 
 export async function logoutUser() {
   
