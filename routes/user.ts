ee555ce13662e3b09663da8e9df1564e7f56de51
LucioFurnari <luciofurnari99@gmail.com>
2024-06-06 23:41:04 -0300

    feat: create new session route

--- routes/user.ts
+++ routes/user.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createUser, loginUser } from '../controllers/user';
+import { createUser, loginUser, verifyToken } from '../controllers/user';
 const router = express.Router();
 
 router.post('/user', createUser);
 
 router.post('/login', loginUser);
 
+router.post('/session', verifyToken)
 export const user = router;
