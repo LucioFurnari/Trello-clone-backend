75f009c0de99e219b2287130312e3ae1079de92d
LucioFurnari <luciofurnari99@gmail.com>
2024-06-09 22:53:43 -0300

    fix: change import of router

--- index.ts
+++ index.ts
@@ -3,11 +3,12 @@ import cors from 'cors';
 import { index } from './routes/index';
 const app = express();
 const PORT = process.env.PORT;
+const { userRouter, workspaceRouter } = index;
 
 app.use(cors());
 
 app.use(express.json());
-app.use('/api', index.user);
+app.use('/api', userRouter, workspaceRouter);
 
 export const server = app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
