142562b976a0e3edda33808aee0673a9faa9daf5
LucioFurnari <luciofurnari99@gmail.com>
2024-06-10 14:01:59 -0300

    feat: import update workspace function and create put route

--- routes/workspace.ts
+++ routes/workspace.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import { verifyToken } from '../controllers/user';
-import { createWorkSpace, getWorkSpace } from '../controllers/workspace';
+import { createWorkSpace, getWorkSpace, updateWorkspace } from '../controllers/workspace';
 
 const router = express.Router();
 
 router.get('/workspace/:workspace_id', getWorkSpace);
 
 router.post('/workspace', verifyToken, createWorkSpace);
+
+router.put('/workspace/:workspace_id', updateWorkspace);
 
 export {router as workspaceRouter}
