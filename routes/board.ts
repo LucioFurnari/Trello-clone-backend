f2e10db8798f0f3438b816124f6ea2021dad8e91
LucioFurnari <luciofurnari99@gmail.com>
2024-06-10 23:23:47 -0300

    feat: import and use create board function, add create board route

--- routes/board.ts
+++ routes/board.ts
@@ -1,7 +1,9 @@
 import express from 'express';
-import { getBoard } from '../controllers/board';
+import { getBoard, createBoard } from '../controllers/board';
 
 const router = express.Router();
+
+router.post('/workspace/:workspace_id/board', createBoard);
 
 router.get('/board/:board_id', getBoard);
 
