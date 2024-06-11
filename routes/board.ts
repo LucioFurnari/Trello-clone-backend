8096b40eee9e7555b1ce600c6af123cf39d23a4c
LucioFurnari <luciofurnari99@gmail.com>
2024-06-11 14:00:48 -0300

    feat: import delete board, crete delete board route

--- routes/board.ts
+++ routes/board.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getBoard, createBoard, updateBoard } from '../controllers/board';
+import { getBoard, createBoard, updateBoard, deleteBoard } from '../controllers/board';
 
 const router = express.Router();
 
@@ -8,5 +8,7 @@ router.post('/workspace/:workspace_id/board', createBoard);
 router.get('/board/:board_id', getBoard);
 
 router.put('/board/:board_id', updateBoard);
+
+router.delete('/board/:board_id', deleteBoard);
 
 export {router as boardRouter};
