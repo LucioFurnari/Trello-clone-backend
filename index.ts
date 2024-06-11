917a67c03d16442405183f8568111f9dba35547b
LucioFurnari <luciofurnari99@gmail.com>
2024-06-11 12:13:36 -0300

    feat: import board router

--- index.ts
+++ index.ts
@@ -3,12 +3,12 @@ import cors from 'cors';
 import { index } from './routes/index';
 const app = express();
 const PORT = process.env.PORT;
-const { userRouter, workspaceRouter } = index;
+const { userRouter, workspaceRouter, boardRouter } = index;
 
 app.use(cors());
 
 app.use(express.json());
-app.use('/api', userRouter, workspaceRouter);
+app.use('/api', userRouter, workspaceRouter, boardRouter);
 
 export const server = app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
