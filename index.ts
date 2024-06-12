b0b2c1490432aa92c351d770202997187e86ebe1
LucioFurnari <luciofurnari99@gmail.com>
2024-06-11 23:34:44 -0300

    feat: add list router

--- index.ts
+++ index.ts
@@ -3,12 +3,12 @@ import cors from 'cors';
 import { index } from './routes/index';
 const app = express();
 const PORT = process.env.PORT;
-const { userRouter, workspaceRouter, boardRouter } = index;
+const { userRouter, workspaceRouter, boardRouter, listRouter } = index;
 
 app.use(cors());
 
 app.use(express.json());
-app.use('/api', userRouter, workspaceRouter, boardRouter);
+app.use('/api', userRouter, workspaceRouter, boardRouter, listRouter);
 
 export const server = app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
