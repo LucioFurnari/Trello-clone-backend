4bac5baebe34c4df3724843957a9224522792f20
LucioFurnari <luciofurnari99@gmail.com>
2024-06-11 23:34:08 -0300

    feat: import list router

--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,11 @@
 import { userRouter } from './user'
 import { workspaceRouter } from './workspace';
 import { boardRouter } from './board';
+import { listRouter } from './list';
 
 export const index = {
   userRouter,
   workspaceRouter,
-  boardRouter
+  boardRouter,
+  listRouter
 };
