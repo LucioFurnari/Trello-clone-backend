07da02c8c2e9b7143da8c358125fcdee6af96b47
LucioFurnari <luciofurnari99@gmail.com>
2024-06-13 23:00:08 -0300

    feat: add function to change position of list

--- controllers/list.ts
+++ controllers/list.ts
@@ -28,3 +28,89 @@ export const createList = [
     return res.status(400).json({ message: 'The board id is not valid', error: true });
   }
 ]
+
+export async function changePosition(req: Request, res: Response) {
+  const { moveTo } = req.body;
+  const { board_id, list_id } = req.params;
+
+  const selectedList = await prisma.list.findUnique({
+    where: {
+      list_id: parseInt(list_id)
+    }
+  })
+
+  console.log(selectedList)
+  if (selectedList?.position! > moveTo) {
+    const updateMultipleListPosition = prisma.list.updateMany({
+      where: {
+        boardId: parseInt(board_id),
+        AND: [
+          {
+            position: {
+              gte: moveTo,
+            },
+          },
+          {
+            position: {
+              lt: selectedList?.position,
+            }
+          }
+        ]
+      },
+      data: {
+        position: {
+          increment: 1
+        }
+      }
+    })
+  
+    const updateListPosition =  prisma.list.update({
+      where: {
+        list_id: parseInt(list_id),
+      },
+      data: {
+        position: parseInt(moveTo),
+      }
+    })
+  
+    await prisma.$transaction([updateMultipleListPosition, updateListPosition]);
+  
+    res.status(200).json({ message: 'Updated'})
+  } else {
+    const updateMultipleListPosition = prisma.list.updateMany({
+      where: {
+        boardId: parseInt(board_id),
+        AND: [
+          {
+            position: {
+              gt: selectedList?.position,
+            },
+          },
+          {
+            position: {
+              lte: moveTo,
+            }
+          }
+        ]
+      },
+      data: {
+        position: {
+          decrement: 1
+        }
+      }
+    })
+  
+    const updateListPosition =  prisma.list.update({
+      where: {
+        list_id: parseInt(list_id),
+      },
+      data: {
+        position: parseInt(moveTo),
+      }
+    })
+  
+    await prisma.$transaction([updateMultipleListPosition, updateListPosition]);
+  
+    res.status(200).json({ message: 'Updated'})
+  }
+};
