1822a11fe0a8d13b1a6c18afdf3b46ac8ab98aa8
LucioFurnari <luciofurnari99@gmail.com>
2024-06-18 12:04:10 -0300

    feat: import and use cookie parse middleware

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { index } from './routes/index';
+import cookieParser from 'cookie-parser';
 const app = express();
 const PORT = process.env.PORT;
 const { userRouter, workspaceRouter, boardRouter, listRouter } = index;
@@ -8,6 +9,7 @@ const { userRouter, workspaceRouter, boardRouter, listRouter } = index;
 app.use(cors());
 
 app.use(express.json());
+app.use(cookieParser())
 app.use('/api', userRouter, workspaceRouter, boardRouter, listRouter);
 
 export const server = app.listen(PORT, () => {
