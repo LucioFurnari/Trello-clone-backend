a5e052aa4d1998f1d6d9f91ce212b292e324c0ca
LucioFurnari <luciofurnari99@gmail.com>
2024-06-18 12:03:40 -0300

    feat: create logout route

--- routes/user.ts
+++ routes/user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createUser, loginUser, verifyToken } from '../controllers/user';
+import { createUser, loginUser, verifyToken, logout } from '../controllers/user';
 const router = express.Router();
 
 router.post('/user', createUser);
@@ -7,5 +7,7 @@ router.post('/user', createUser);
 router.post('/login', loginUser);
 
 router.post('/session', verifyToken)
+
+router.post('/logout', logout);
 
 export {router as userRouter};
