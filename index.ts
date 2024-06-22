92bcca84091fb8268fe77b4dc248d2d96f9ba0e9
LucioFurnari <luciofurnari99@gmail.com>
2024-06-21 23:58:22 -0300

    feat: import member route

--- index.ts
+++ index.ts
@@ -4,13 +4,13 @@ import { index } from './routes/index';
 import cookieParser from 'cookie-parser';
 const app = express();
 const PORT = process.env.PORT;
-const { userRouter, workspaceRouter, boardRouter, listRouter } = index;
+const { userRouter, workspaceRouter, boardRouter, listRouter, memberRouter } = index;
 
 app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser())
-app.use('/api', userRouter, workspaceRouter, boardRouter, listRouter);
+app.use('/api', userRouter, workspaceRouter, boardRouter, listRouter, memberRouter);
 
 export const server = app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
