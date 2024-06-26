615ff748e62cac7591bae8ece7468542313dab43
LucioFurnari <luciofurnari99@gmail.com>
2024-06-26 18:50:58 -0300

    feat: create post card route

--- routes/card.ts
+++ routes/card.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { createCard } from "../controllers/card";
 const router = Router();
+
+router.post('/list/:listId/card', createCard);
 
 export {router as cardRouter};
