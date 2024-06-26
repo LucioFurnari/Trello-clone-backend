83de20c779bc77564f921aaf14b58813b5b25ff5
LucioFurnari <luciofurnari99@gmail.com>
2024-06-26 18:51:30 -0300

    feat: import and use cardRouter

--- index.ts
+++ index.ts
@@ -4,13 +4,13 @@ import { index } from './routes/index';
 import cookieParser from 'cookie-parser';
 const app = express();
 const PORT = process.env.PORT;
-const { userRouter, workspaceRouter, boardRouter, listRouter, memberRouter } = index;
+const { userRouter, workspaceRouter, boardRouter, listRouter, memberRouter, cardRouter } = index;
 
 app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser())
-app.use('/api', userRouter, workspaceRouter, boardRouter, listRouter, memberRouter);
+app.use('/api', userRouter, workspaceRouter, boardRouter, listRouter, memberRouter, cardRouter);
 
 export const server = app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
