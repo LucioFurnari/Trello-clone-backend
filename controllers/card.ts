292dd2771cd86b72fc1bd51564dd3d60b29130cb
LucioFurnari <luciofurnari99@gmail.com>
2024-06-25 23:55:36 -0300

    feat: import prisma and create card

--- controllers/card.ts
+++ controllers/card.ts
@@ -1,14 +1,24 @@
 import { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export async function getCard(_req: Request, _res: Response) {
   // Get card 
 }
 
 export async function createCard(req: Request, res: Response) {
-  const { title, description, startDate, dueDate } = req.body;
+  const { title, description, startDate, dueDate, listId } = req.body;
 
-  
+  const card = await prisma.card.create({
+    data: {
+      title: title,
+      listId: listId
+    }
+  })
+
+  return res.status(201).json({ message: 'Card created', card})
 }
 
 export async function updateCard() {
