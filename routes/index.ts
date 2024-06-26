968b9cf8740a9422a0693663c33e7b25994e6a2a
LucioFurnari <luciofurnari99@gmail.com>
2024-06-26 18:51:14 -0300

    feat: import cardRouter

--- routes/index.ts
+++ routes/index.ts
@@ -3,11 +3,13 @@ import { workspaceRouter } from './workspace';
 import { boardRouter } from './board';
 import { listRouter } from './list';
 import { memberRouter } from './member';
+import { cardRouter } from './card';
 
 export const index = {
   userRouter,
   workspaceRouter,
   boardRouter,
   listRouter,
-  memberRouter
+  memberRouter,
+  cardRouter
 };
