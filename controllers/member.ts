b9703aa58c91404405dd607e49d61bc72778200f
LucioFurnari <luciofurnari99@gmail.com>
2024-06-25 22:05:02 -0300

    feat: add try catch

--- controllers/member.ts
+++ controllers/member.ts
@@ -6,14 +6,18 @@ const prisma = new PrismaClient();
 export async function addMember(req: Request, res: Response) {
   const { workspace_id } = req.params;
   const { user_id } = req.body;
-
-  const addedUser = await prisma.workspaceUsers.create({
-    data: {
-      is_admin: false,
-      userId: user_id,
-      workspaceId: parseInt(workspace_id)
-    }
-  });
-
-  return res.status(201).json({ message: 'User added to the workspace', addedUser });
+  try {
+    const addedUser = await prisma.workspaceUsers.create({
+      data: {
+        is_admin: false,
+        userId: user_id,
+        workspaceId: parseInt(workspace_id)
+      }
+    });
+  
+    return res.status(201).json({ message: 'User added to the workspace', addedUser });
+  } catch (error) {
+    console.error('Error fetching workspace:', error);
+    return res.status(500).json({ message: 'Internal server error', error: true });
+  }
 }
