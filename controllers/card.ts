0716266dd54011c4301656ae0e670d6f15e7605b
LucioFurnari <luciofurnari99@gmail.com>
2024-06-26 19:30:17 -0300

    feat: implement validation, catch error, validate list

--- controllers/card.ts
+++ controllers/card.ts
@@ -8,18 +8,46 @@ export async function getCard(_req: Request, _res: Response) {
   // Get card 
 }
 
-export async function createCard(req: Request, res: Response) {
-  const { title, description, startDate, dueDate, listId } = req.body;
+export const createCard = [
+  body('title').notEmpty().trim().escape(),
+  body('description').trim().escape(),
+  async (req: Request, res: Response) => {
+    const { title, description, startDate, dueDate, coverColor, coverImage } = req.body;
+    const { listId } = req.params;
 
-  const card = await prisma.card.create({
-    data: {
-      title: title,
-      listId: listId
+    const result = validationResult(req);
+
+    if(!result.isEmpty()){
+      return res.status(400).json({ errors: result.array()});
     }
-  })
+    try {
+    const list = await prisma.list.findUnique({
+      where: {
+        listId: parseInt(listId)
+      }
+    });
 
-  return res.status(201).json({ message: 'Card created', card})
-}
+    if (!list) return res.status(404).json({ message: 'List not found', error: true});
+
+    const card = await prisma.card.create({
+      data: {
+        title: title,
+        listId: parseInt(listId),
+        description: description,
+        startDate: startDate,
+        dueDate: dueDate,
+        coverColor: coverColor,
+        coverImage: coverImage
+      }
+    });
+
+    return res.status(201).json({ message: 'Card created', card})
+    } catch (error) {
+      console.error('Error fetching workspace:', error);
+      return res.status(500).json({ message: 'Internal server error', error: true });
+    }
+  }
+];
 
 export async function updateCard() {
   // Update card
