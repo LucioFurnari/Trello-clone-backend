95c6da7e614ab4d0c25746f16074c08cb7cabdcb
LucioFurnari <luciofurnari99@gmail.com>
2024-06-26 22:36:03 -0300

    feat(change) change params names

--- controllers/board.ts
+++ controllers/board.ts
@@ -4,8 +4,13 @@ import { body, validationResult } from 'express-validator';
 
 const prisma = new PrismaClient();
 
-export async function getBoard(_req: Request, _res: Response) {
-  // Get board from request.board_id
+export async function getBoard(req: Request, _res: Response) {
+  const { boardId } = req.params;
+  const board = await prisma.board.findUnique({
+    where: {
+      boardId: parseInt(boardId)
+    }
+  })
 }
 
 export const createBoard = [
@@ -14,7 +19,7 @@ export const createBoard = [
   async (req: Request, res: Response) => {
     const result = validationResult(req);
     const { title,  description } = req.body;
-    const { workspace_id } = req.params;
+    const { workspaceId } = req.params;
 
     if(result.isEmpty()) {
       try {
@@ -22,7 +27,7 @@ export const createBoard = [
           data: {
             title: title,
             description: description,
-            workspaceId: parseInt(workspace_id)
+            workspaceId: parseInt(workspaceId)
           }
         });
         return res.status(200).json({ board });
@@ -40,17 +45,17 @@ export const updateBoard = [
   body('description').escape(),
   async (req: Request, res: Response) => {
     const result = validationResult(req);
-    const { board_id } = req.params;
+    const { boardId } = req.params;
     const { title, description } = req.body;
 
     if (!result.isEmpty()) {
       return res.status(400).json({ errorList: result.array() });
     }
     
-    if (!Number.isNaN(parseInt(board_id))) {
+    if (!Number.isNaN(parseInt(boardId))) {
       const updatedBoard = await prisma.board.update({
         where: {
-          boardId: parseInt(board_id),
+          boardId: parseInt(boardId),
         },
         data: {
           title: title,
@@ -65,12 +70,12 @@ export const updateBoard = [
 ]
 
 export async function deleteBoard(req: Request, res: Response) {
-  const { board_id } = req.params;
+  const { boardId } = req.params;
 
-  if(!Number.isNaN(parseInt(board_id))) {
+  if(!Number.isNaN(parseInt(boardId))) {
     const deletedBoard = await prisma.board.delete({
       where: {
-        boardId: parseInt(board_id),
+        boardId: parseInt(boardId),
       }
     });
     return res.status(200).json({ message: 'Board deleted successfully', deletedBoard});
