feda7c9e2758249cab9ce15b43ac3f6cea77ee86
LucioFurnari <luciofurnari99@gmail.com>
2024-06-27 19:59:58 -0300

    feat: add new routes for card

--- routes/card.ts
+++ routes/card.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
-import { createCard } from "../controllers/card";
+import { createCard, deleteCard, updateCard, getCard } from "../controllers/card";
 const router = Router();
 
 router.post('/list/:listId/card', createCard);
+
+router.delete('/card/:cardId', deleteCard);
+
+router.put('/card/:cardId', updateCard);
+
+router.get('/card/:cardId', getCard);
 
 export {router as cardRouter};
