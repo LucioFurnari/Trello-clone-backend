3f21ff6e4ee01b19c368efa642272ff4e9237c7c
LucioFurnari <luciofurnari99@gmail.com>
2024-07-04 20:12:14 -0300

    feat: change file name, remove utils files, change folder name

---
+++ interfaces/interfaces.ts
@@ -0,0 +1,27 @@
+import { Request } from "express";
+
+export interface AuthRequest extends Request {
+  user: {
+    username: string,
+    email: string, 
+    id: number
+  }
+}
+
+export interface JwtPayload {
+  username: string,
+  email: string,
+  id: number
+}
+
+export interface NewBoardEntry {
+  title: string,
+  description?: string,
+  coverColor?: string,
+  coverImage?: string
+}
+
+export interface NewCardEntry extends NewBoardEntry {
+  startDate: string,
+  dueDate: string,
+}
