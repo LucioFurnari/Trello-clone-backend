f327fae03fc5f9659044b47b8965ded263f3b18f
LucioFurnari <luciofurnari99@gmail.com>
2024-07-04 18:37:08 -0300

    feat: import unsplashRouter

--- index.ts
+++ index.ts
@@ -4,13 +4,13 @@ import { index } from './routes/index';
 import cookieParser from 'cookie-parser';
 const app = express();
 const PORT = process.env.PORT;
-const { userRouter, workspaceRouter, boardRouter, listRouter, memberRouter, cardRouter } = index;
+const { userRouter, workspaceRouter, boardRouter, listRouter, memberRouter, cardRouter, unsplashRouter } = index;
 
 app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser())
-app.use('/api', userRouter, workspaceRouter, boardRouter, listRouter, memberRouter, cardRouter);
+app.use('/api', userRouter, workspaceRouter, boardRouter, listRouter, memberRouter, cardRouter, unsplashRouter);
 
 export const server = app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
