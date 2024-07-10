603a03b6939d9dca0ea2638c30c34d016e192fe2
LucioFurnari <luciofurnari99@gmail.com>
2024-07-10 19:07:30 -0300

    feat: change cors options

--- index.ts
+++ index.ts
@@ -6,10 +6,10 @@ const app = express();
 const PORT = process.env.PORT;
 const { userRouter, workspaceRouter, boardRouter, listRouter, memberRouter, cardRouter, unsplashRouter } = index;
 
-app.use(cors());
-
+app.use(cors({ credentials: true }));
 app.use(express.json());
 app.use(cookieParser())
+
 app.use('/api', userRouter, workspaceRouter, boardRouter, listRouter, memberRouter, cardRouter, unsplashRouter);
 
 export const server = app.listen(PORT, () => {
