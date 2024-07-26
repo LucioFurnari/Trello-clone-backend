7b4d66258aea70a8677ef26559c6021ae14f710c
LucioFurnari <luciofurnari99@gmail.com>
2024-07-25 21:44:48 -0300

    feat: add delete list route

--- routes/list.ts
+++ routes/list.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { createList, changePosition } from "../controllers/list";
+import { createList, changePosition, deleteList } from "../controllers/list";
 
 const router = Router();
 
 router.post('/board/:boardId/list', createList);
 
-router.put('/board/:boardId/list/:listId', changePosition)
+router.put('/board/:boardId/list/:listId', changePosition);
+
+router.delete('/list/:listId', deleteList);
 
 export { router as listRouter }
