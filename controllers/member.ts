c79ef15039f592bec13d9eee458d1a4b5d38a902
LucioFurnari <luciofurnari99@gmail.com>
2024-08-07 21:06:54 -0300

    feat: change controller add more validations

--- controllers/member.ts
+++ controllers/member.ts
@@ -5,18 +5,32 @@ import { Request, Response } from "express";
 export async function addMember(req: Request, res: Response) {
   const { workspaceId } = req.params;
   const { userId } = req.body;
+
+  // Validate input
+  if (!workspaceId || !userId) {
+    return res.status(400).json({ message: 'Workspace ID and User ID are required' });
+  }
+
   try {
+    // Add user to workspace
     const addedUser = await prisma.workspaceUsers.create({
       data: {
         is_admin: false,
-        userId: userId,
-        workspaceId: parseInt(workspaceId)
+        userId,
+        workspaceId
       }
     });
-  
+
     return res.status(201).json({ message: 'User added to the workspace', addedUser });
   } catch (error) {
-    console.error('Error fetching workspace:', error);
-    return res.status(500).json({ message: 'Internal server error', error: true });
+    console.error('Error adding user to workspace:', error);
+    
+    // Handle different error types
+    if (error instanceof Error) {
+      return res.status(500).json({ message: 'Internal server error', error: error.message });
+    }
+
+    // Fallback in case error is not an instance of Error
+    return res.status(500).json({ message: 'Internal server error', error: 'Unknown error' });
   }
 }
