7e0f715e1ab274d22d5ee21aa533cabbb70cd256
LucioFurnari <luciofurnari99@gmail.com>
2024-10-06 23:41:07 -0300

    feat: change function to return only image data

--- controllers/unsplash.ts
+++ controllers/unsplash.ts
@@ -1,17 +1,24 @@
-import axios from "axios";
 import { Request, Response } from "express";
-
 
 export async function getUnsplashImages(req: Request, res: Response) {
   try {
-    const response = await axios.get(`https://api.unsplash.com/search/photos?page=${req.params.pageNumber}&query=scenery`, {
+    const response = await fetch(`https://api.unsplash.com/photos?page=${req.params.pageNumber}&query=scenery`, {
       headers: {
         Authorization: `Client-ID ${process.env.UNSPLASH_DEV_KEY}`
       },
-      params: req.query
     });
 
-    return res.status(200).json(response.data);
+    const images = await response.json();
+
+    // Filter out only the image data you need (e.g., id, urls, description)
+    const filteredImages = images.map((image: any) => ({
+      id: image.id,
+      urls: image.urls,
+      alt_description: image.alt_description,
+      // Add any other fields you want to keep
+    }));
+
+    return res.status(200).json(filteredImages);
   } catch (error) {
     console.error('Failed to fetch images from Unsplash');
     return res.status(500).json({ message: 'Failed to fetch images from Unsplash', error})
