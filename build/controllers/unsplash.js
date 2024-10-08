45af3c7c2c37ba41be48124ef1b9759bf5579f39
LucioFurnari <luciofurnari99@gmail.com>
2024-10-08 20:38:23 -0300

    feat: update build folder

--- build/controllers/unsplash.js
+++ build/controllers/unsplash.js
@@ -8,22 +8,25 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getUnsplashImages = void 0;
-const axios_1 = __importDefault(require("axios"));
 function getUnsplashImages(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const response = yield axios_1.default.get(`https://api.unsplash.com/search/photos?page=${req.params.pageNumber}&query=scenery`, {
+            const response = yield fetch(`https://api.unsplash.com/photos/random?count=${req.params.pageNumber}&query=nature`, {
                 headers: {
                     Authorization: `Client-ID ${process.env.UNSPLASH_DEV_KEY}`
                 },
-                params: req.query
             });
-            return res.status(200).json(response.data);
+            const images = yield response.json();
+            // Filter out only the image data you need (e.g., id, urls, description)
+            const filteredImages = images.map((image) => ({
+                id: image.id,
+                urls: image.urls,
+                alt_description: image.alt_description,
+                // Add any other fields you want to keep
+            }));
+            return res.status(200).json(filteredImages);
         }
         catch (error) {
             console.error('Failed to fetch images from Unsplash');
