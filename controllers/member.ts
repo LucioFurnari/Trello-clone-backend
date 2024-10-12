8d968f897a9e310314f3ba23c0d66a88359b8359
LucioFurnari <luciofurnari99@gmail.com>
2024-10-12 02:48:03 -0300

    feat: change the code for add user to workspace

--- controllers/member.ts
+++ controllers/member.ts
@@ -1,36 +1,38 @@
 import prisma from "../models/prismaClient";
 import { Request, Response } from "express";
 
-
-export async function addMember(req: Request, res: Response) {
-  const { workspaceId } = req.params;
+export async function addUserToWorkspace(req: Request, res: Response) {
   const { userId } = req.body;
+  const { workspaceId } = req.params;
 
-  // Validate input
-  if (!workspaceId || !userId) {
-    return res.status(400).json({ message: 'Workspace ID and User ID are required' });
+  if (!userId || !workspaceId) {
+    return res.status(400).json({ error: 'User ID and Workspace ID are required' });
   }
 
   try {
-    // Add user to workspace
-    const addedUser = await prisma.workspaceUsers.create({
-      data: {
-        is_admin: false,
-        userId,
-        workspaceId
-      }
+    const existingEntry = await prisma.workspaceUsers.findUnique({
+      where: {
+        userId_workspaceId: {
+          userId: userId,
+          workspaceId: workspaceId
+        },
+      },
     });
 
-    return res.status(201).json({ message: 'User added to the workspace', addedUser });
-  } catch (error) {
-    console.error('Error adding user to workspace:', error);
-    
-    // Handle different error types
-    if (error instanceof Error) {
-      return res.status(500).json({ message: 'Internal server error', error: error.message });
+    if (existingEntry) {
+      return res.status(400).json({ error: 'User is already a member of this workspace'});
     }
 
-    // Fallback in case error is not an instance of Error
-    return res.status(500).json({ message: 'Internal server error', error: 'Unknown error' });
+    const newWorkspaceUser = await prisma.workspaceUsers.create({
+      data: {
+        userId: userId,
+        workspaceId: workspaceId
+      },
+    });
+
+    return res.status(201).json(newWorkspaceUser);
+  } catch (error) {
+    console.error('Error adding user to workspace:', error);
+    return res.status(500).json({ error: 'Internal server error' });
   }
-}
+};
